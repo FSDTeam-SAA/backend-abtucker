438ebdd7a4e22f53eba0d7f37029d86953ad6a30
zihadulislam2 <zihadul708@gmail.com>
2025-10-17 04:31:03 +0600

    update theme.controller.ts

--- src/theme/theme.controller.ts
+++ src/theme/theme.controller.ts
@@ -5,18 +5,27 @@ import {
   UseInterceptors,
   UploadedFile,
   Body,
+  Res,
 } from '@nestjs/common';
 import { ThemeService } from './theme.service';
 import { CreateThemeDto } from './dto/theme.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { sendResponse } from '../common/utils/sendResponse';
+import type { Response } from 'express';
 
 @Controller('theme')
 export class ThemeController {
   constructor(private readonly themeService: ThemeService) {}
 
   @Get()
-  async getTheme() {
-    return this.themeService.getTheme();
+  async getTheme(@Res() res: Response) {
+    const theme = await this.themeService.getTheme();
+    return sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Theme fetched successfully',
+      data: theme,
+    });
   }
 
   @Post()
@@ -24,7 +33,19 @@ export class ThemeController {
   async createOrUpdateTheme(
     @Body() body: CreateThemeDto,
     @UploadedFile() logo?: Express.Multer.File,
+    @Res() res?: Response,
   ) {
-    return this.themeService.createOrUpdateTheme(body, logo);
+    const theme = await this.themeService.createOrUpdateTheme(body, logo);
+    if (!res) {
+      // handle the case when res is undefined
+      return;
+    }
+
+    return sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: 'Theme created/updated successfully',
+      data: theme,
+    });
   }
 }
