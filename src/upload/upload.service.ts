e07c582cf8a17786c304204ba74229c573174eec
zihadulislam2 <zihadul708@gmail.com>
2025-10-18 05:42:10 +0600

    update upload.service.ts

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -10,12 +10,14 @@ export class UploadService {
       const stream = cloudinary.uploader.upload_stream(
         { folder: 'nest_uploads' },
         (error, result) => {
+          // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
           if (error) return reject(error);
           if (!result) return reject(new Error('No result from Cloudinary'));
           resolve(result);
         },
       );
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       streamifier.createReadStream(file.buffer).pipe(stream);
     });
   }
@@ -28,47 +30,3 @@ export class UploadService {
     return Promise.all(uploadPromises);
   }
 }
-
-// import { Injectable } from '@nestjs/common';
-// import { UploadApiResponse } from 'cloudinary';
-// import toStream from 'buffer-to-stream';
-// import cloudinary from '../config/cloudinary.config';
-
-// @Injectable()
-// export class UploadService {
-//   // 👉 Single file upload
-//   async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
-//     console.log('File:', file.originalname, file.mimetype, file.size);
-//     console.log('Cloudinary config:', cloudinary.config());
-
-//     return new Promise((resolve, reject) => {
-//       const upload = cloudinary.uploader.upload_stream(
-//         { folder: 'nest_uploads' },
-//         (error: unknown, result: UploadApiResponse | undefined) => {
-//           if (error) {
-//             const errMessage =
-//               error instanceof Error ? error.message : 'Unknown error';
-//             reject(new Error(errMessage));
-//             return;
-//           }
-//           if (!result) {
-//             reject(new Error('No result from Cloudinary'));
-//             return;
-//           }
-//           resolve(result);
-//         },
-//       );
-
-//       (toStream(file.buffer) as NodeJS.ReadableStream).pipe(upload);
-//     });
-//   }
-
-//   // 👉 Multiple file upload
-//   async uploadImages(
-//     files: Express.Multer.File[],
-//   ): Promise<UploadApiResponse[]> {
-//     const uploadPromises = files.map((file) => this.uploadImage(file));
-
-//     return Promise.all(uploadPromises);
-//   }
-// }
